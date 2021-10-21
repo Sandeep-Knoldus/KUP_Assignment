9a42283aea444c1d1d3d5d83c98e1e8c3856881f
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-10-21 14:32:42 +0530

    Removed Warnings

--- Assignments/Assignment-6/src/test.rs
+++ Assignments/Assignment-6/src/test.rs
@@ -1,26 +1,53 @@
-#[warn(unused_imports)]
 #[cfg(test)]
 #[test]
-fn _generating_substring_test1() {
-    use crate::question_1::generate::_generate_substring;
-    assert_eq!(_generate_substring("pa".to_string()), ["p", "pa", "a"]);
+pub fn generating_substring_test1() {
+    use crate::question_1::generate::generate_substring;
+    assert_eq!(generate_substring("pa".to_string()), ["p", "pa", "a"]);
 }
 
 #[test]
-fn _search_substring_test1() {
-    use crate::question_1::search::_search;
-    assert_eq!(_search("Pankaj Chaudhury", "Cha"), "7".to_string());
+pub fn generating_substring_test2() {
+    use crate::question_1::generate::generate_substring;
+    assert_ne!(
+        generate_substring("sit".to_string()),
+        ["s", "i", "t", "si", "it"]
+    );
 }
 
 #[test]
-fn _max_min_test1() {
-    use crate::question_2::string_manipulate::_output;
+pub fn search_substring_test1() {
+    use crate::question_1::search::search;
+    assert_eq!(search("Pankaj Chaudhury", "Cha"), "7".to_string());
+}
+
+#[test]
+pub fn search_substring_test2() {
+    use crate::question_1::search::search;
+    assert_ne!(search("Sandeep Chakraborty", "Cha"), "1".to_string());
+}
+
+#[test]
+pub fn max_min_test1() {
+    use crate::question_2::string_manipulate::output;
     assert_eq!(
-        _output(
+        output(
             "jjdhid".to_string(),
             "ikjhjk".to_string(),
             "rtysgi".to_string()
         ),
         "itdsgk".to_string()
+    );
+}
+
+#[test]
+pub fn max_min_test2() {
+    use crate::question_2::string_manipulate::output;
+    assert_ne!(
+        output(
+            "jjdhid".to_string(),
+            "ikjhjk".to_string(),
+            "rtysgi".to_string()
+        ),
+        "jirdki".to_string()
     );
 }
